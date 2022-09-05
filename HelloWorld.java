b4b1c866fed75242643ee458dbfefa3400079629
Austin Rush <arush2@stu.jsu.edu>
2022-09-04 23:56:14 -0500

    Added f2()

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 /**
  *
- * @author Your Name
+ * @author Austin Rush
  */
 
 public class HelloWorld {
@@ -13,6 +13,10 @@ public class HelloWorld {
 
     private void f1() {
         // v1.0
+    }
+        
+    private void f2() {
+        // v2.0
     }
 
 }
